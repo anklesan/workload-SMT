44787b46062b0c27ddeaadae970141eafd0b03e5
Aidan B <aidanb1049@gmail.com>
2026-05-27 12:44:22 -0700

    Load workers

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from z3 import *
+import json
+
+
+class Worker:
+    def __init__(self, worker_id: str, device_name: str, provides: dict, location: str):
+        self.worker_id = worker_id
+        self.device_name = device_name
+        self.provides = provides
+        self.location = location
+
+
+def load_workers(fpath: str) -> list[Worker]:
+    with open(fpath, "r") as file:
+        worker_jsons = json.load(file)
+
+    return [Worker(**worker_json) for worker_json in worker_jsons]
+
+
 def main():
-    print("Hello from workload-smt!")
+    workers = load_workers("data/workers.json")
+    for w in workers:
+        print(w)
 
 
 if __name__ == "__main__":
